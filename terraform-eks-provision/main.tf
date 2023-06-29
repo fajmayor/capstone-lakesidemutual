4e122d5711e6a78eb59979f601c8648951e50d25
fajmayor <fajmayor@gmail.com>
2023-06-29 11:28:08 +0100

    Apply

--- terraform-eks-provision/main.tf
+++ terraform-eks-provision/main.tf
@@ -8,8 +8,8 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
- # cluster_name = "lakesidemutual-eks-${random_string.suffix.result}"
-  cluster_name = "lakesidemutual-eks"
+  cluster_name = "lakesidemutual-eks-${random_string.suffix.result}"
+ # cluster_name = "lakesidemutual-eks"
 }
 
 resource "random_string" "suffix" {
