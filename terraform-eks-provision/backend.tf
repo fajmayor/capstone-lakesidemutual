6acd3a70ab068432ba683a397f9980cc5e423118
fajmayor <fajmayor@gmail.com>
2023-07-08 13:45:05 +0100

    Terraform Apply

--- terraform-eks-provision/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket  = "microservice-project-terraform-state"
-    key     = "microservice-project-terraform.tfstate"
-    region  = "us-west-2"
-    profile = "fajmayor"
-  }
-}
