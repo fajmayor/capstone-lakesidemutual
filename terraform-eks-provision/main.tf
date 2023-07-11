0f6d290bf26eb6b784a23b11073a17173b0b5487
fajmayor <fajmayor@gmail.com>
2023-07-11 19:06:39 +0100

    Terraform Apply

--- terraform-eks-provision/main.tf
+++ terraform-eks-provision/main.tf
@@ -52,11 +52,12 @@ module "eks" {
   cluster_version = "1.27"
 
   vpc_id                         = module.vpc.vpc_id
-  subnet_ids                     = module.vpc.public_subnets
+  subnet_ids                     = module.vpc.private_subnets
   cluster_endpoint_public_access = true
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
+    mapPublicIpOnLaunch = true
 
   }
 
@@ -79,7 +80,6 @@ module "eks" {
       min_size     = 1
       max_size     = 2
       desired_size = 1
-      mapPublicIpOnLaunch = true
     }
   }
 }
