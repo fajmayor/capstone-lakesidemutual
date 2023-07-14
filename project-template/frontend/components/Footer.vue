6254d0774e52b33b6afbadd0d671cca28dd59416
Mayowa Fajube <fajmayor@gmail.com>
2023-07-14 20:42:23 +0100

    Remove Dir

--- project-template/frontend/components/Footer.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <v-footer padless>
-      <v-col class="text-center" cols="12">
-        {{ new Date().getFullYear() }} —
-        <strong>Antonio Payá González</strong>
-      </v-col>
-    </v-footer>
-</template>
